935d5f532c533eea9c19cbf3fd81a0b8098bc9b8
alsbtain <info@alsbtain.net>
2016-05-31 03:11:17 -0700

    custimization

--- www/settings.js
+++ www/settings.js
@@ -1,1 +1,6 @@
 var siteURL = 'http://www.al-3bbas.com/';
+
+var externalMenu = { 
+    title: ["الموقع الرسمي للحسينية", "حساب الحسينية على اليوتيوب", "صفحة الحسينية على الفيس بوك"],
+    url: ["http://www.al-3bbas.com/", "https://www.youtube.com/user/al3bbastube", "https://www.facebook.com/%D8%AD%D8%B3%D9%8A%D9%86%D9%8A%D8%A9-%D8%A3%D8%A8%D9%8A-%D8%A7%D9%84%D9%81%D8%B6%D9%84-%D8%A7%D9%84%D8%B9%D8%A8%D8%A7%D8%B3-%D8%B9%D9%84%D9%8A%D9%87-%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85-%D8%A8%D8%AD%D9%8A-%D8%A7%D9%84%D8%AA%D8%B1%D9%83%D9%8A%D8%A9-154138474641411/"]
+};
