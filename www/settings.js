a780807c4cc5a6d0c9dff3253fa813d4ba5222c1
alsbtain <info@alsbtain.net>
2016-07-02 03:37:16 -0700

    UPGRADE

--- www/settings.js
+++ www/settings.js
@@ -3,6 +3,11 @@ var siteURL = 'http://www.al-3bbas.com/';
 // var shareURL = siteURL + 'media_redirect.php';
 var shareURL = 'http://sbta.in/al3bbas.php';
 
+var internalMenu = { 
+    title: ["الأخبار", "المقالات", "مكتبة الصور", "مكتبة الميديا", "مجلد التحميلات"],
+    url: ["news.html", "news.html?articles=1", "gallery.html", "media.html", "downloads.html"]
+};
+
 var externalMenu = { 
     title: ["الموقع الرسمي للحسينية", "حساب الحسينية على اليوتيوب", "صفحة الحسينية على الفيس بوك"],
     url: ["http://www.al-3bbas.com/", "https://www.youtube.com/user/al3bbastube", "https://www.facebook.com/%D8%AD%D8%B3%D9%8A%D9%86%D9%8A%D8%A9-%D8%A3%D8%A8%D9%8A-%D8%A7%D9%84%D9%81%D8%B6%D9%84-%D8%A7%D9%84%D8%B9%D8%A8%D8%A7%D8%B3-%D8%B9%D9%84%D9%8A%D9%87-%D8%A7%D9%84%D8%B3%D9%84%D8%A7%D9%85-%D8%A8%D8%AD%D9%8A-%D8%A7%D9%84%D8%AA%D8%B1%D9%83%D9%8A%D8%A9-154138474641411/"]
